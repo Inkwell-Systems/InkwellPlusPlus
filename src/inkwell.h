9b62f02293b72b48bdf7cb0d1d3053b07f000d9a
FreeDownloadHere <cristachecarol13@gmail.com>
2023-07-20 22:30:48 +0300

    Delete src dir

--- src/inkwell.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "inkwellEnums.h"
-#include "inkwellEntry.h"
-#include "inkwellTable.h"
-#include "inkwellProject.h"
-#include "inkwellDeserializer.h"
-#include "inkwellSerializer.h"
--- src/inkwellProject.cpp
+++
@@ -1,3 +0,0 @@
-#include "inkwellProject.h"
-
-using namespace inkwell;
